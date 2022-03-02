45e644fe042039a3f4b01855b18e39af0593e836
Axel Vidoni <vidoni.axel@gmail.com>
2022-03-02 14:08:08 +0100

    add main center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { MainCenterComponent } from './main-center/main-center.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarMenuComponent,
+    MainCenterComponent
   ],
   imports: [
     BrowserModule,
