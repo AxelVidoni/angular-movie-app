cb906107be4ac71f38ce072557abd4aab06e89e3
Axel Vidoni <vidoni.axel@gmail.com>
2022-03-02 15:49:30 +0100

    Correction bug

--- src/app/main-center/main-center.component.ts
+++ src/app/main-center/main-center.component.ts
@@ -9,12 +9,25 @@ import { Film } from '../type';
 export class MainCenterComponent implements OnInit {
   films : Film[] = [
     {
-       id: 1,
-    
-      picture: "https://image.tmdb.org/t/p/w780//1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+      adult: false,
+      genre_ids: [28,12,878],
+       id: 634649,
+      original_language: "en",
+      original_title: "Spider-Man: No Way Home",
+      overview : "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+      popularity: 5944.171,
+      release_date: '2021-12-15',
       title: 'Spider-Man: No Way Home',
+      video: false,
+      vote_average: 8.3,
+      vote_count: 8569,
+      poster_path: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+
+      backdrop_path: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg",
+      
       //  description: "C'est beau une ville la nuit !"
-    }
+    },
+
   ] ;
   constructor() { }
 
--- src/app/type.ts
+++ src/app/type.ts
@@ -1,6 +1,18 @@
 export class Film {
+    adult!: boolean;
+    genre_ids!: number[];
     id!: number;
+    original_language !: string;
+    original_title!: string;
+    overview!: string;
+    popularity!: number;
+    release_date!: string;
     title!: string;
+    video !: boolean;
+    vote_average!: number;
+    vote_count!: number;
+    poster_path!: string;
     // description!: string;
-    picture!: string;
+    backdrop_path!: string;
   }
+  
