9244108e0960ccc1ff2cce055e497785940f141c
Axel Vidoni <vidoni.axel@gmail.com>
2022-03-02 14:18:34 +0100

    correction projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 import { MainCenterComponent } from './main-center/main-center.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SidebarMenuComponent,
     MainCenterComponent
   ],
